caf82fc66fcb560d6e064903f003085c4afe5712
BlackMATov <blackmat@mail.ru>
2023-01-05 08:52:04 +0700

    Happy New Year 🥳

--- untests/flat_set_tests.cpp
+++ untests/flat_set_tests.cpp
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * This file is part of the "https://github.com/blackmatov/flat.hpp"
  * For conditions of distribution and use, see copyright notice in LICENSE.md
- * Copyright (C) 2019-2022, by Matvey Cherevko (blackmatov@gmail.com)
+ * Copyright (C) 2019-2023, by Matvey Cherevko (blackmatov@gmail.com)
  ******************************************************************************/
 
 #include <flat.hpp/flat_set.hpp>
